688d05b221adfbfe27699f53b896949ca128b2ea
Nathaniel May <nathanieldmay@gmail.com>
2019-12-01 15:15:11 -0500

    making distribution test deterministic

--- src/test/scala-2.13/shuffle/ShuffleUnitTest.scala
+++ src/test/scala-2.13/shuffle/ShuffleUnitTest.scala
@@ -1,10 +1,10 @@
 package shuffle
 
-import org.scalatest._
-import shuffle.FunctionalShuffle.shuffle
-
+import scala.annotation.tailrec
 import scala.math.abs
 import scala.util.Random
+import shuffle.all._
+import org.scalatest._
 
 
 class ShuffleUnitTest extends FlatSpec with Matchers {
@@ -14,19 +14,52 @@ class ShuffleUnitTest extends FlatSpec with Matchers {
     val tolerance = .05
     val samples   = scala.math.pow(2 * size / tolerance, 2).toInt
     val goal      = samples.toDouble/factorial(size)
-    val input: LazyList[LazyList[Int]] = LazyList.tabulate(samples)(_ => (0 until size).iterator.to(LazyList))
 
-    val map = (for {
-      lazyList <- input
-      key    =  shuffle(lazyList)
-    } yield key)
-      .foldLeft[Map[List[Int], Int]](Map()) {
-        (m, rand) => {
-          val k = rand.eval(new Random(System.nanoTime())).toList
-          m.updated(k, m.getOrElse(k, 0) + 1) } }
+    // randomly generated seeds. Using the same static seeds makes the test deterministic.
+    val seeds = List(
+      6481974435664621795L,
+      4791688558456143830L,
+      293875002857805281L,
+      7733768347870080636L,
+      6871359988984629593L,
+      6629083326245445978L,
+      1967586611012001563L,
+      6860531089457037514L,
+      1552289256667894481L,
+      2553103043961892635L
+    )
 
-      withClue(s"all values must be within (${min(goal, tolerance)}, ${max(goal, tolerance)})\n") {
-        map.values.filter(notWithin(goal, tolerance)) should be (empty) }
+    val failedSeeds = seeds.view
+      .map(maxValue(samples, size, tolerance, goal))
+      .zip(seeds)
+      .filter(x => notWithin(goal, tolerance)(x._1))
+      .map(_._2)
+      .toList
+
+    withClue(s"all values must be within (${min(goal, tolerance)}, ${max(goal, tolerance)})\nThe following seeds failed: ${failedSeeds.mkString(", ")}") {
+      failedSeeds shouldBe empty
+    }
+  }
+
+  def maxValue(samples: Int, size: Int, tolerance: Double, goal: Double)(seed: Long): Int = {
+    // prevents the stack overflow that comes with the State monad sequence & eval approach.
+    // definitely a better solution out there than relying on external mutation.
+    val mutR = new Random(seed)
+
+    @tailrec
+    def agg[A](input: LazyList[Rand[LazyList[A]]], r: Random, m: Map[List[A], Int]): Map[List[A], Int] = input match {
+      case LazyList() => m
+
+      case x #:: xs =>
+        val k = x.eval(r).toList
+        agg(xs, r, m.updated(k, m.getOrElse(k, 0) + 1))
+    }
+
+    val input =
+      LazyList.tabulate(samples) { _ => (0 until size).iterator.to(LazyList) }
+        .map(shuffle)
+
+    agg[Int](input, mutR, Map()).values.max
   }
 
   def min(goal: Double, tol: Double): Double = goal - goal*abs(tol)
